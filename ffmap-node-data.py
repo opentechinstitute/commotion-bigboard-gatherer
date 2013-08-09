874954a2a7ca843acb82c0b5c3e1054913693773
Nat Meysenburg <nat@stealthisemail.com>
2013-08-09 17:35:44 -0400

    Move url loading, to add in better error checking to keep the script from crashing hard if it can't reach a node.

--- ffmap-node-data.py
+++ ffmap-node-data.py
@@ -5,7 +5,7 @@ import urllib2
 import collections
 
 # TODO determine this on the fly or statically set in a more reasonable way
-pwd = '/home/bigboard/ffhmap/'
+pwd = '/home/bigboard/commotion-bigboard-gatherer/'
 
 # create some sets arrays, etc, for passing data around
 ip_mac_map = collections.defaultdict(lambda: None, {})
@@ -14,14 +14,6 @@ nodes = []
 
 # load the config file
 config = json.loads(open(pwd +'bigboard-server-config', 'r').read())
-
-# this function takes a url of the json file, and returns a decoded json obj
-def decode_json(url):
-    try: 
-        ret = json.loads(urllib2.urlopen(url).read())
-        return ret
-    except (ValueError, KeyError, TypeError):
-        return "JSON format error"
 
 # this function takes decoded json from decode_json() and parses out the node
 # data. additionally, this function will build an IP MAC address table.
@@ -74,7 +66,19 @@ def parse_node(decoded_json):
 # we have gone through all the nodes, and recorded their mac addresses.
 for ip in open(pwd +'monitored-nodes', 'r').readlines():
     url = 'http://'+ip+':9090/all'
-    nodes.append(parse_node(decode_json(url)))
+    try: 
+        pulled_json = urllib2.urlopen(url).read()        
+    except:
+        pulled_json = None
+        
+    if pulled_json is not None:
+        try:
+            decoded = json.loads(pulled_json)
+        except (ValueError, KeyError, TypeError):
+            decoded = None
+
+    if decoded is not None:
+        nodes.append(parse_node(decoded))
 
 # d3 actually needs consistent indexing so we need to add that to our map
 for i, node_data in enumerate(nodes):
